edd6f242e885e3e658f16b7ad9998afdee2b171c
Bill Nickel <bill.nickel@gmail.com>
2023-09-29 11:30:11 -0700

    Move averaging logic to function.

--- C/Arrays/average.c
+++ C/Arrays/average.c
@@ -5,6 +5,9 @@ gcc average.c -o average.out
 ./average.out
 */
 
+// need to define the function and implement below
+float array_average(int length, int array[]);
+
 int main(void) 
 {
     int score1 = 72; // 4 bytes
@@ -13,24 +16,32 @@ int main(void)
 
     printf("Average: %f\n", (score1 + score2 + score3) / 3.0f);
 
-    // use an array instead of declaring arbitrary amounts of variables.
     int scores[] = { 72, 73, 33 }; // 12 contiguous bytes.
 
-    int i; 
-    int total;
-    int scores_length = sizeof(scores) / sizeof(scores[0]);
+    // a couple of different ways to get length of array
+    int array_length = sizeof(scores) / sizeof(scores[0]);
     int pointer_length = *(&scores + 1) - scores;
 
-    printf("Array length: %i\n", scores_length);
-    printf("Pointer length: %i\n", pointer_length);
+    float average = array_average(array_length, scores);
 
-    for (i = 0; i < scores_length; ++i) 
+    printf("Average: %f\n", average);
+    return 0;
+}
+
+// you have to pass the length of the array here
+// because it doesn't seem possible to use the
+// above means to get the length of the array.
+float array_average(int length, int array[]) 
+{
+    int total;
+
+    for (int i = 0; i < length; ++i) 
     {
-        total += scores[i];
-        printf("Total: %i, %i\n", total, scores[i]);
+        total += array[i];
+        printf("Total: %i, %i\n", total, array[i]);
     }
 
     printf("Total: %i\n", total);
-    printf("Average: %f\n", (float)total / scores_length);
-    return 0;
+
+    return (float)total / length;
 }
