9e5993cba891116e8c4406eb5108ffc03ea237d0
Bill Nickel <bill.nickel@gmail.com>
2023-10-01 08:43:25 -0700

    Add quick sort and binary search.

--- C/Arrays/array_search.c
+++ C/Arrays/array_search.c
@@ -9,9 +9,37 @@ typedef struct
 }
 Person;
 
+// compare for Person name
+int comparePersonName(const void *s1, const void *s2) 
+{
+    /*
+        IMPORTANT!!!!
+
+        The array being sorted is an array of
+        pointers so we have pointers to pointers
+        and need to use ** here to get the pointer
+        to Person.
+
+        https://stackoverflow.com/questions/3903849/issue-with-qsort-the-sorting-is-not-properly-done-c
+    */
+    Person * person1 = *(Person **)s1;
+    Person * person2 = *(Person **)s2;
+
+    return strcmp(person1->name, person2->name);
+}
+
+// compare for Person age
+int comparePersonAge(const void *s1, const void *s2) {
+    Person * person1 = *(Person **)s1;
+    Person * person2 = *(Person **)s2;
+
+    return person1->age - person2->age;
+}
+
 // function prototypes
 Person* createPerson(char* name, int age);
 Person* linearFind(int peopleLength, Person* people[], char* name);
+Person * binaryFind(Person * people[], char * name, int low, int high);
 
 int main(void)
 {
@@ -22,16 +50,40 @@ int main(void)
 
     // array is of pointers to Person structs
     Person * people[] = { one, two, three, four };
-    Person * tanner = linearFind(4, people, "Tanner");
+
+    // sort people by name
+    // My first segmentation fault :) Keeping here for nostalgia.
+    // qsort(people, 4, sizeof(Person), comparePersonName);
+    qsort(people, 4, sizeof(one), comparePersonName);
+
+    // are the People sorted?
+    for (int i = 0; i < 4; i++) 
+    {
+        printf("Person %i: %s\n", i + 1, people[i]->name);
+    }
+
+    Person * tanner = linearFind(5, people, "Tanner");
 
     if (tanner) 
     {
-        printf("Found Tanner. He is %i years old.\n", tanner->age);
+        printf("Found Tanner with linear search. They are %i years old.\n", tanner->age);
     }
     else 
     {
-        printf("Could not find Tanner.");
+        printf("Could not find Tanner with linear search.\n");
     }
+
+    Person * michelle = binaryFind(people, "Michelle", 0, 3);
+
+    if (michelle) 
+    {
+        printf("Found Michelle with binary search. They are %i years old.\n", michelle->age);
+    }
+    else 
+    {
+        printf("Could not find Michelle with binary search.\n");
+    }
+
 }
 
 Person * createPerson(char * name, int age) 
@@ -66,5 +118,38 @@ Person * linearFind(int peopleLength, Person * people[], char * name)
     }
 
     // is returning a null pointer bad?
+    return NULL;
+}
+
+Person * binaryFind(Person * people[], char * name, int low, int high) 
+{
+    if (high >= low)
+    {
+        // get the mid point of array. what about even length arrays?
+        int mid = low + (high - low) / 2;
+        printf("Mid point is %i\n", mid);
+
+        int checkMid = strcmp(people[mid]->name, name);
+        printf("Mid compare for %s is %i\n", people[mid]->name, checkMid);
+
+        // if name at mid matches search, return it
+        if (checkMid == 0)
+        {
+            return people[mid];
+        }
+
+        // search left
+        if (checkMid > 0)
+        {
+            return binaryFind(people, name, low, mid - 1);
+        }
+
+        // search right
+        if (checkMid < 0)
+        {
+            return binaryFind(people, name, mid + 1, high);
+        }
+    }
+
     return NULL;
 }
