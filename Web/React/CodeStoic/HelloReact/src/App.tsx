da3833784798d0daa13db0ab0ab15619c69adbe4
Bill Nickel <bill.nickel@gmail.com>
2023-12-28 07:42:40 -0800

    Learning React

---
+++ Web/React/CodeStoic/HelloReact/src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // NOTE: from what I understand, this is bad because changes to this state will cause a re-render of the entire
+  //       DOM. React doesn't know if children (and their children) need this state so it naively re-renders recursively.
+  const [count, setCount] = useState(0)
+
+  // NOTE: The empty <></> tags are required to group the output because a React component
+  //       can only return 1 element.
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
